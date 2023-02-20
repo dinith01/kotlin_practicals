8dea4d41d190112a0dd38365feec06e5af28f00e
dinith01 <d.01himash@yahoo.com>
2023-02-20 16:21:43 +0530

    array's 1st element changed

--- arrays.kt
+++ arrays.kt
@@ -1,8 +1,19 @@
+
+
 fun main(){
 
     var fruits = arrayOf("apple","grapes","pineapple")
 
-    println(fruits.size)
+    //print array size
+    println("fruits array size is : ${fruits.size}")
+
+    //print 4th element array
+    println ("fruits array 4th value is: ${fruits[2]} ")
+
+    //change 1st element value
+    fruits[1] = "Mango"
+
+
 
 
 }
